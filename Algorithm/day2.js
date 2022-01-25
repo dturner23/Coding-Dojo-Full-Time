ffb5f65344df57a1e882c5f334be0a482371c9c6
Dustin Turner <dustin.b.turner@outlook.com>
2022-01-25 10:52:18 -0600

    day 2 morning commit

---
+++ Algorithm/day2.js
@@ -0,0 +1,26 @@
+
+//Predict Loops
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+    i = i + 3;
+}
+
+console.log("outside of the loop " + i);
+
+//Lesson 3 - predict what the code does
+
+// function getTotal(arrayOfNumbers) {
+
+//     var sum = arrayOfNumbers[0];
+
+//     for (var i = 0; i < arrayOfNumbers.length; i++) {
+//         sum += arrayOfNumbers[i];
+//         console.log("the current sum is: " + sum);
+//     }
+
+//     console.log("the total is: " + sum);
+
+// }
+
+// getTotal([1, 3, 5]);
