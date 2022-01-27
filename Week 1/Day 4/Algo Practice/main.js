32fee9196770d0c464493f794617b60c839db1c0
Dustin Turner <dustin.b.turner@outlook.com>
2022-01-27 12:25:28 -0600

    mid day progress commit

--- Week 1/Day 4/Algo Practice/main.js
+++ Week 1/Day 4/Algo Practice/main.js
@@ -1,14 +1,31 @@
 //1 - Code Flow Functions
 
+
+//When we create a variable outside of a function, is has global scope.
+//When we create a variable inside of a function, is has local scope.
+
+//Functions are a series of steps that accomplish a task.
+//Functions are reusable blocks of code that can be called to perform a task.
+//Function declartion:
+//Starts with the funtion keyword, followed by the name of the function.
+//The function name is followed by the parameters.
+//Followed by the function's code block.
+
+
 var x = 5;
     
 function setX(newValue) {
-    x = newValue; 
+    x = newValue; //Sets the value of x to the value of newValue and becomes global.
 }
     
-console.log(x);
-setX(15);
-console.log(x);
+console.log(x); //-->5
+
+//Invoking or calling a function
+//Functions are called by using the function name followed by parentheses.
+
+setX(15); //-->15
+
+console.log(x); //-->15
 
 
 var x = 5;
@@ -18,10 +35,10 @@ function addToX(amount) {
     console.log("hello there");
 }
     
-console.log(x);
-var result = addToX(-10);
-console.log(result);
-console.log(x);
+console.log(x); // --> 5
+var result = addToX(-10); 
+console.log(result); // -->-5
+console.log(x); // -->5
 
 
 //2 - Code Flow - is the array a palindrome?
