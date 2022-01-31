bcdd142abc60b80145f4c6de2aca9de03ad62e5e
Dustin Turner <dustin.b.turner@outlook.com>
2022-01-31 08:57:18 -0600

    morning progress commit

---
+++ Week 2/Day 1/Button Clicker/main.js
@@ -0,0 +1,17 @@
+// Function to change button text from Login to Logout
+
+function changeButton(element) {
+    element.innerText = "Logout";
+}
+
+//Function to remove the Add Definition button
+
+function removeButton(element) {
+    element.remove()
+} 
+
+//Function to add alert modal on like button push
+
+function alertButton(element) {
+    alert("Ninja was Liked")
+}
