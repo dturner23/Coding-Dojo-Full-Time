c4fa13f6b2f23a537557332b0a115d16c93ddd77
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-05 12:42:47 -0600

    intro to python

--- Week 2/Day 4/Algo Practice/script.js
+++ Week 2/Day 4/Algo Practice/script.js
@@ -1,28 +1,46 @@
 // Nesting Arrays
 
-var arr2d = [ [2, 5, 8],
-              [3, 6, 1],
-              [5, 7, 7] ];
-    
+var arr2d = [[2, 5, 8],
+            [3, 6, 1],
+            [5, 7, 7]];
+
+function isPresent2d(arr2d, value){
+    for (let i = 0; i < arr2d.length; i++){
+        if (value == arr2d[i][j]){
+            return console.log(true);
+        } else {
+            console.log(false);
+        }
+    }
+}
+
 // We can console.log the `8` in this array if we
-console.log(arr2d[0][2]);
+
+console.log(arr2d[0][2]); // calling the parent and the child
+
 // the first index `0` will select the `[2, 5, 8]` sub-array
+
 // the second index `2` will select the `8` out of that sub-array
 
 // Flatten Array
+
+//Built in methods. Come from pre-baked JS.
+// .push() - adds to the end of an array
+// .pop() - removes from the end of an array
+
 
 // complete the following function
 function flatten(arr2d) {
     var flat = [];
     // your code here
-    for (var i = 0; i < arr2d.length; i++) {
-        for (var j = 0; j < arr2d[i].length; j++) {
-            flat.push(arr2d[i][j]);
-            }
+    for (var i = 0; i < arr2d.length; i++) {  // loop through the outer array
+        for (var j = 0; j < arr2d[i].length; j++) {  // loop through the inner array
+            flat.push(arr2d[i][j]);  // push the inner array's elements to the end of the flat array
         }
+    }
     return flat;
 }
-    
-var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+
+var result = flatten([[2, 5, 8], [3, 6, 1], [5, 7, 7]]);
 console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
 
