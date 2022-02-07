41a3af178ea713924ea1070ab45ff16f1d18ab7d
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-07 08:02:57 -0600

    day 1 morning stuff

---
+++ Python/fundamentals/introduction/examples.py
@@ -0,0 +1,14 @@
+import random
+print(random.randint(2,5)) # provides a random number between 2 and 5
+
+
+name = "Zen"
+print("My name is", name)  # adding a comma after the name will add a space
+
+name = "Zen"
+print("My name is " + name) # adding a + will add a space
+
+
+
+
+
