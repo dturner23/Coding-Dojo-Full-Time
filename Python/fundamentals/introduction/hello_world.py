4c3dca32690822f3b292e0400a36363d76a08df1
Dustin Turner <dustin.b.turner@outlook.com>
2022-02-08 08:23:56 -0600

    morning practice commit

---
+++ Python/fundamentals/introduction/hello_world.py
@@ -0,0 +1,25 @@
+# 1 print "hello world"
+
+print("hello world")
+
+#2 print "Hello Noelle!" with the name in a variable
+
+name = "Noelle"
+
+print("Hello", name, "!")
+print("Hello " + name + "!")
+
+#3 print "Hello42!" with the number in a variable
+
+name = 42
+
+print("Hello", name, "!")
+print("Hello " + name + "!")
+
+#4 print "I love to eat sushi and pizza." with the foods in variables with .format and f strings
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
